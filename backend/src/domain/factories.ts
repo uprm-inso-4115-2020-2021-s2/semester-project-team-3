ba75fd8d38c5c0f5b354de4a715555d049f7fbdb
Jose D. Maldonado <josedmaldonado14@gmail.com>
2021-02-18 12:03:59 -0400

    added implementations

--- backend/src/domain/factories.ts
+++ backend/src/domain/factories.ts
@@ -1,25 +1,31 @@
 import { IAppointment, IBaseReview, ICarListing, IClient, IListingReview, IRenteeReview, ITransaction } from "./declarations";
+import { Appointment } from "./implementations/appointment";
+import { CarListing } from "./implementations/carlisting";
+import { Client } from "./implementations/client";
+import { ListingReview } from "./implementations/listingreview";
+import { RenteeReview } from "./implementations/renteereview";
+import { Transaction } from "./implementations/transaction";
 
 export function makeClient(data: Partial<IClient>): IClient {
-    throw Error("Not implemented yet.")
+    return new Client(data)
 }
 
 export function makeCarListing(data: Partial<ICarListing>): ICarListing {
-    throw Error("Not implemented yet.")
+    return new CarListing(data)
 }
 
 export function makeTransaction(data: Partial<ITransaction>): ITransaction {
-    throw Error("Not implemented yet.")
+    return new Transaction(data)
 }
 
 export function makeAppointment(data: Partial<IAppointment>): IAppointment {
-    throw Error("Not implemented yet.")
+    return new Appointment(data)
 }
 
 export function makeRenteeReview(data: Partial<IRenteeReview>): IRenteeReview {
-    throw Error("Not implemented yet.")
+    return new RenteeReview(data)
 }
 
 export function makeListingReview(data: Partial<IListingReview>): IListingReview {
-    throw Error("Not implemented yet.")
+    return new ListingReview(data)
 }
