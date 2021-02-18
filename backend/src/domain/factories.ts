db836540628b30634fe61026fb6d9735b7f114ea
Jose D. Maldonado <josedmaldonado14@gmail.com>
2021-02-18 11:58:24 -0400

    factoriando

---
+++ backend/src/domain/factories.ts
@@ -0,0 +1,25 @@
+import { IAppointment, IBaseReview, ICarListing, IClient, IListingReview, IRenteeReview, ITransaction } from "./declarations";
+
+export function makeClient(data: Partial<IClient>): IClient {
+    throw Error("Not implemented yet.")
+}
+
+export function makeCarListing(data: Partial<ICarListing>): ICarListing {
+    throw Error("Not implemented yet.")
+}
+
+export function makeBaseReview(data: Partial<ITransaction>): ITransaction {
+    throw Error("Not implemented yet.")
+}
+
+export function makeAppointment(data: Partial<IAppointment>): IAppointment {
+    throw Error("Not implemented yet.")
+}
+
+export function makeRenteeReview(data: Partial<IRenteeReview>): IRenteeReview {
+    throw Error("Not implemented yet.")
+}
+
+export function makeListingReview(data: Partial<IListingReview>): IListingReview {
+    throw Error("Not implemented yet.")
+}
